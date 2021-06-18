d018d54406bb5ef596bf55199ca2bf6eb1e996a9
Parth099 <parthnpatel715@gmail.com>
2021-06-18 12:37:57 -0400

    added continuation

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ var operandB = {
     decimal: false,
 };
 
-var operator;
+var operator, prevOp;
 var writeToNumber = operandA;
 
 var operatorHash = {
@@ -24,6 +24,12 @@ var operatorHash = {
     3: "*",
 }
 
+function resetOperand(o){
+    o.number = "";
+    o.sign = 1;
+    o.signStr = "";
+    decimal: false
+}
 function operandToString(o){
     return `${o.signStr}${o.number}`;
 }
@@ -34,17 +40,24 @@ function pushNum(digit){
     writeToNumber.number += digit;
     let ns = document.querySelector("#output > #number-space");
     ns.textContent = (writeToNumber.signStr + writeToNumber.number) ?? 0;
-
 }
 
 function pushOp(op){
+    let ns = document.querySelector("#output > #number-space");
+    let hs = document.querySelector("#output > #history");
+
+    prevOp = operator
     operator = operatorHash[op];
     writeToNumber = operandB ;
 
-    let ns = document.querySelector("#output > #number-space");
-    ns.textContent = "0"
+    
+    hs.textContent = `${operandToString(operandA)} ${operator}`
+    //ns.textContent = "0"
 
-    let hs = document.querySelector("#output > #history");
+    if(writeToNumber.number && prevOp){
+        performCalc(operandA, operandB, prevOp);
+        prevOp = undefined;
+    }
     hs.textContent = `${operandToString(operandA)} ${operator}`
 }
 
@@ -61,9 +74,11 @@ function performCalcWrapper(){
 }
 
 function performCalc(numA, numB, op){
+
+    console.log(operandToString(numA), operandToString(numB), op)
+
     let a = parseFloat(operandToString(numA));
     let b = parseFloat(operandToString(numB));
-    console.log(op);
     let mainStr = ""
     
     if(op == "+"){
@@ -78,12 +93,29 @@ function performCalc(numA, numB, op){
     else if(op == "/"){
         mainStr += a / b;
     }
-    console.log(a + b);
     pushResult(mainStr);
 }
 
+function writeResToOperand(result, o, neg = false){
+    if(result[0] == '-'){
+        writeResToOperand(result.slice(1), o, neg = true)
+    }
+    o.number = result.slice(neg);
+    o.decimal = (result.includes(".")) ? true : false
+
+    o.sign  = (neg) ? -1 : 1;
+    o.signStr = (o.sign == 1) ? "" : "-";
+
+}
+
+function moveToNextOp(result){
+    writeResToOperand(result, operandA)
+    resetOperand(operandB)
+}
 function pushResult(str){
     let hs = document.querySelector("#output > #history");
     hs.textContent = `${operandToString(operandA)} ${operator} ${operandToString(operandB)} =`
     document.querySelector("#output > #number-space").textContent = str;
+
+    moveToNextOp(str)
 }
