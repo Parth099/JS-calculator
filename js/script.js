4b75edbf2f1c17199d16fc0b2e669931eb8ec45d
Parth099 <parthnpatel715@gmail.com>
2021-06-20 12:55:34 -0400

    added CSS Styling for buttons and number output added Decimal and EXPO support added decimal trunc added clear and reset added Error Messages & notices added New Images for buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //declare constants
-
 var operandA = {
     number: "",
     sign: 1,
@@ -16,6 +15,7 @@ var operandB = {
 
 var operator, prevOp;
 var writeToNumber = operandA;
+var last_res;
 
 var operatorHash = {
     0: "+",
@@ -47,19 +47,27 @@ function pushNum(digit){
 }
 
 function pushNumWrapper(sp){
-    let btn = document.getElementById("decimal")
-    if(sp == '.' && !writeToNumber.decimal){
-        writeToNumber.decimal ^= 1;
-        pushNum(".")
-        btn.classList.add("disabled")
-    }
-    if(sp == 'r'){
-        btn.classList.remove("disabled")
+    if(writeToNumber.number.length < 13){
+        let btn = document.getElementById("decimal")
+        if(sp == '.' && !writeToNumber.decimal){
+            console.log("hit")
+            writeToNumber.decimal = true;
+            pushNum(".")
+            btn.classList.add("disabled")
+        }
+        else if(sp == 'r'){
+            btn.classList.remove("disabled")
+            writeToNumber.decimal = false;
+        }
+        else{
+            pushNum((sp == '.') ? "" : sp)
+        }
     }
 }
 
 function pushOp(op){
     pushNumWrapper("r")
+    showMessage("")
     let ns = document.querySelector("#output > div.ns-cont > #number-space");
     ns.textContent = "_";
     let hs = document.querySelector("#output > #history");
@@ -99,6 +107,7 @@ function popNumber(){
 function pushSign(){
     writeToNumber.sign *= -1;
     writeToNumber.signStr = (writeToNumber.sign == 1) ? "" : "-";
+    document.querySelector("#output > #history").textContent = last_res ?? "0";
     pushNum("") //simply updates display
 }
 
@@ -109,9 +118,6 @@ function performCalcWrapper(){
 }
 
 function performCalc(numA, numB, op){
-    
-    console.log(operandToString(numA), operandToString(numB), op)
-
     if(!numA.number){
         numA.number = "0"
     }
@@ -135,7 +141,15 @@ function performCalc(numA, numB, op){
     else if(op == "^"){
         mainStr += Math.pow(a, b)
     }
-    pushResult(mainStr);
+    if(mainStr == "NaN"){
+        clearCalc();
+        showMessage("Operation Unsupported");
+    }
+    else{
+        let result = truncNumber(mainStr);
+        last_res = result;
+        pushResult(result);
+    }
 }
 
 function writeResToOperand(result, o, neg = false){
@@ -158,6 +172,43 @@ function pushResult(str){
     let hs = document.querySelector("#output > #history");
     hs.textContent = `${operandToString(operandA)} ${operator} ${operandToString(operandB)} = `
     document.querySelector("#output > div.ns-cont > #number-space").textContent = str;
-
     moveToNextOp(str)
+}
+
+//math
+function truncNumber(num){ //num  --> str
+   if(num.includes("e")){
+       //expo mode active
+       let loc = num.indexOf("e");
+       showMessage("Number has been truncated,\n Precision reduced");
+       return num.slice(0, Math.min(9, loc)) + num.slice(loc)
+       
+   }
+   else if(num.length > 10){
+       max = Math.min(10, num.length);
+       showMessage("Number has been truncated,\n Precision reduced");
+       return parseFloat(num.slice(0, max)).toExponential();
+   }
+   return num;
+}
+
+function clearCalc(){
+    console.log("clear");
+    resetOperand(operandA);
+    resetOperand(operandB)
+    operator = undefined;
+    writeToNumber = operandA;
+    pushNumWrapper("")
+    document.querySelector("#output > #history").textContent = "0";
+}
+
+function showMessage(str){
+    let p = document.querySelector(".msg");
+    p.textContent = str;
+}
+
+function log(){
+    writeToNumber.number = truncNumber(Math.log(parseFloat(writeToNumber.number))+"0")
+    console.log(writeToNumber)
+    pushNum("") //empty update to display log
 }
